49c91e0ab0e22c86cff40c1f150f3a270b17b24b
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-03-08 15:31:06 -0500

    Configured dev database

--- shambomon/config/dev.exs
+++ shambomon/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :shambomon, Shambomon.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "shambomon",
+  password: "paiDooc8oe6w",
   database: "shambomon_dev",
   hostname: "localhost",
   pool_size: 10
