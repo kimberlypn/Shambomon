420825342ae68754be525dc02f7046540f45ddf2
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-03-16 01:09:59 -0400

    Rendered battlefield

--- shambomon/assets/js/shambomon.jsx
+++ shambomon/assets/js/shambomon.jsx
@@ -12,6 +12,10 @@ class Shambomon extends React.Component {
     this.channel = props.channel;
     this.state = {
       availableCharacters: [],
+      p1Char: "Bulbasaur",
+      p2Char: "Charmander",
+      p1Health: 100,
+      p2Health: 100
     };
 
     this.channel.join()
@@ -29,7 +33,56 @@ class Shambomon extends React.Component {
   render() {
     return(
       <div>
+        <div>
+          <div class="row player-info">
+            <PlayerInfo player={1} state={this.state} />
+            <Player img={"/images/" + this.state.p1Char + "-battle.png"} />
+          </div>
+        </div>
+        <div class="row player-info" id="player-2">
+          <Player img={"/images/" + this.state.p2Char + "-battle.png"} />
+          <PlayerInfo player={2} state={this.state} />
+        </div>
       </div>
     );
   }
+}
+
+function Player(props) {
+  return (
+    <div class="col-3">
+      <img src={props.img}/>
+    </div>
+  );
+}
+
+// Renders a player's character and health
+function PlayerInfo(props) {
+  var health, name, pokemon, pos;
+  if (props.player == 1) {
+    health = props.state.p1Health;
+    name = props.state.p1Char;
+  }
+  else {
+    health = props.state.p2Health;
+    name = props.state.p2Char;
+  }
+  var hp = []
+  for (var i = 0; i < health; i++) {
+    hp.push(<HP />);
+  }
+  return (
+    <div class="col-9">
+      <p>{name}</p>
+      <div class="hp">
+        <p>HP</p>
+        {hp}
+        <p>{health} / 100</p>
+      </div>
+    </div>
+  )
+}
+
+function HP(props) {
+  return <div class="hp-bar"></div>;
 }
