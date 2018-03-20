b9c29c9761cd32746cc9f4d6d3a12e9921992790
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-03-20 00:30:08 -0400

    Added password validation to user changeset

--- shambomon/lib/shambomon/accounts/user.ex
+++ shambomon/lib/shambomon/accounts/user.ex
@@ -6,13 +6,47 @@ defmodule Shambomon.Accounts.User do
   schema "users" do
     field :username, :string
 
+    field :password_hash, :string
+    field :pw_tries, :integer
+    field :pw_last_try, :utc_datetime
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username])
-    |> validate_required([:username])
+    |> cast(attrs, [:username, :password, :password_confirmation])
+    |> unique_constraint(:username)
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
+    |> validate_required([:username, :password_hash])
   end
+
+  # Password validation
+  # From Comeonin docs
+  def validate_password(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, password ->
+      case valid_password?(password) do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || msg}]
+      end
+    end)
+  end
+
+  def put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+  def put_pass_hash(changeset), do: changeset
+
+  def valid_password?(password) when byte_size(password) > 7 do
+    {:ok, password}
+  end
+
+  def valid_password?(_), do: {:error, "The password is too short"}
 end
--- shambomon/lib/shambomon_web/controllers/session_controller.ex
+++ shambomon/lib/shambomon_web/controllers/session_controller.ex
@@ -6,8 +6,8 @@ defmodule ShambomonWeb.SessionController do
   alias Shambomon.Accounts
   alias Shambomon.Accounts.User
 
-  def create(conn, %{"username" => username}) do
-    user = Accounts.get_user_by_username(username)
+  def create(conn, %{"username" => username, "password" => password}) do
+    user = get_and_auth_user(username, password)
     if user do
       conn
       |> put_session(:user_id, user.id)
@@ -16,6 +16,15 @@ defmodule ShambomonWeb.SessionController do
       conn
       |> put_flash(:error, "The information you’ve entered doesn’t match any account. Please try again.")
       |> redirect(to: page_path(conn, :index))
+    end
+  end
+
+  # TODO: Move to user.ex
+  def get_and_auth_user(username, password) do
+    user = Accounts.get_user_by_username(username)
+    case Comeonin.Argon2.check_pass(user, password) do
+      {:ok, user} -> user
+      _else       -> nil
     end
   end
 
