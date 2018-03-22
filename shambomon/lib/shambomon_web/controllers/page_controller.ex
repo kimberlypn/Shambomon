6a771f541aee9e627fc76a80d4ca73679d64a218
mdang8 <mdang808@gmail.com>
2018-03-22 15:35:57 -0400

    Added more playable character options
    Added 9 additional characters and updated characters EEX template page to render on a loop

--- shambomon/lib/shambomon_web/controllers/page_controller.ex
+++ shambomon/lib/shambomon_web/controllers/page_controller.ex
@@ -10,7 +10,24 @@ defmodule ShambomonWeb.PageController do
   end
 
   def characters(conn, params) do
-    render conn, "characters.html", game: params["game"]
+    characters1 = [
+      %{ name: "Charmander", source: "/images/Charmander.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Charmander-Icon-Free-to-Use-623942829" },
+      %{ name: "Squirtle", source: "/images/Squirtle.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Squirtle-Icon-Free-to-use-623939100" },
+      %{ name: "Bulbasaur", source: "/images/Bulbasaur.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Bulbasaur-Icon-Free-to-Use-623933759" },
+      %{ name: "Pikachu", source: "/images/Pikachu.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Pikachu-Icon-Free-to-use-623946649" },
+      %{ name: "Eevee", source: "/images/Eevee.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Eevee-Icon-Free-to-Use-623949414" },
+      %{ name: "Jigglypuff", source: "/images/Jigglypuff.png", credits: "https://jedflah.deviantart.com/art/Minimalist-Jigglypuff-Icon-Free-to-use-628616339" }
+    ]
+    characters2 = [
+      %{ name: "Onix", source: "/images/Onix.png", credits: "" },
+      %{ name: "Ninetales", source: "/images/Ninetales.png", credits: "" },
+      %{ name: "Gengar", source: "/images/Gengar.png", credits: "" },
+      %{ name: "Articuno", source: "/images/Articuno.png", credits: "" },
+      %{ name: "Zapdos", source: "/images/Zapdos.png", credits: "" },
+      %{ name: "Moltres", source: "/images/Moltres.png", credits: "" }
+    ]
+
+    render conn, "characters.html", game: params["game"], characters1: characters1, characters2: characters2
   end
 
   def name(conn, _params) do
