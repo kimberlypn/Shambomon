c60de63961a13f5f105cd4810ee165eaa76971d1
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-03-26 15:56:06 -0400

    Code cleanup

--- shambomon/assets/js/components/messages.jsx
+++ shambomon/assets/js/components/messages.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 // Returns the messages in the order and format that they should be rendered
 function getMessages(messages) {
   var msgs = [];
-  // Make the newest set of messages a different color
-  for (var i = messages.length - 1; i >= messages.length - 3; i--) {
-    msgs.push(<p className="last-messages">>> {messages[i]}</p>);
-  }
-  if (messages.length >= 3) {
-    msgs.push(<br />);
-    for (var i = messages.length - 4; i >= 0; i--) {
-      msgs.push(<p>>> {messages[i]}</p>);
-      // Add a space between each round
-      if (i % 3 == 0) {
-        msgs.push(<br />);
+  if (messages.length > 0) {
+    // Make the newest set of messages a different color
+    for (var i = messages.length - 1; i >= messages.length - 3; i--) {
+      msgs.push(<p className="last-messages">>> {messages[i]}</p>);
+    }
+    if (messages.length >= 3) {
+      msgs.push(<br />);
+      for (var i = messages.length - 4; i >= 0; i--) {
+        msgs.push(<p>>> {messages[i]}</p>);
+        // Add a space between each round
+        if (i % 3 == 0) {
+          msgs.push(<br />);
+        }
       }
     }
   }
@@ -24,21 +26,11 @@ function getMessages(messages) {
 export default function Messages(props) {
   let messages = props.messages;
   let msgs = getMessages(messages);
-  
-  // Skip if no messages
-  if (messages.length == 0) {
-    return (
-      <div className="col-md-3" id="messages">
-        <h3>ATTACK HISTORY</h3>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div className="col-md-3" id="messages">
-        <h3>ATTACK HISTORY</h3>
-        {msgs}
-      </div>
-    );
-  }
+
+  return (
+    <div className="col-md-3" id="messages">
+      <h3>ATTACK HISTORY</h3>
+      {msgs}
+    </div>
+  );
 }
